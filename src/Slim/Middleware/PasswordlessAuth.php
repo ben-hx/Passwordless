6bef3234384c38043d5ab215540feb6eace9c5e8
Thomas Klokosch <mail@thomasklokosch.com>
2014-08-08 16:46:53 +0200

    Added basic middleware skeleton

---
+++ src/Slim/Middleware/PasswordlessAuth.php
@@ -0,0 +1,33 @@
+<?php
+/*
+* Passwordless token-based authentication
+*
+* Copyright (c) 2014 Thomas Klokosch
+*
+* Licensed under the MIT license:
+* http://www.opensource.org/licenses/mit-license.php
+*
+* Project home:
+* https://github.com/thomasklokosch/slim-passwordless-auth
+*
+*/
+namespace Slim\Middleware;
+
+/*
+ *
+ */
+class PasswordlessAuthentication extends \Slim\Middleware
+{
+
+    public function __construct()
+    {
+
+    }
+
+    public function call()
+    {
+        $this->next->call();
+    }
+
+}
+
