6870f3a922102998f518528c1acb611a02039ad1
Thomas Klokosch <mail@thomasklokosch.com>
2014-10-07 07:09:52 +0200

    Updated tests to include removal of userHash method on Passwordless class

--- tests/PasswordlessTest.php
+++ tests/PasswordlessTest.php
@@ -55,16 +55,6 @@ class PasswordlessTest extends \PHPUnit_Framework_TestCase
     /*
      * @depends testIfPasswordlessCanBeInitialized
      */
-    public function testIfHashCanBeCalculated()
-    {
-        $this->assertNotEmpty( $this->passwordless->getUserHash('test@test.com') );
-        $this->assertEquals('f660ab912ec121d1b1e928a0bb4bc61b15f5ad44d5efdc4e1c92a25e99b8e44a', $this->passwordless->getUserHash('test@test.com') );
-    }
-
-
-    /*
-     * @depends testIfPasswordlessCanBeInitialized
-     */
     public function testIfTokenCanBeRequested()
     {
         $token = $this->passwordless->requestToken('test@test.com');
